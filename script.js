50a2c35c5cf9b94557b785f1bb0fb2db3842dd2f
Isingizwe12 <isingizwealine12@gmail.com>
2025-09-19 21:21:50 +0200

    making all work

--- script.js
+++ script.js
@@ -6,15 +6,47 @@ numberButtons.forEach(btn=>{
     outputInput.value += btn.innerText;
   });
 })
+// operation buttons
+
 let addBtn=document.getElementById("add-btn");
 addBtn.addEventListener("click",function(){
   outputInput.value += "+";
 })
-
+let subBtn=document.getElementById("sub-btn");
+subBtn.addEventListener("click",function(){
+  outputInput.value += "-";
+})
+let mulBtn=document.getElementById("mul-btn");
+mulBtn.addEventListener("click",function(){
+  outputInput.value += "*";
+})
+let diviBtn=document.getElementById("divi-btn");
+diviBtn.addEventListener("click",function(){
+  outputInput.value += "/";
+})
+let moduBtn=document.getElementById("modu-btn");
+moduBtn.addEventListener("click",function(){
+  outputInput.value += "%";
+})
+let dotBtn=document.getElementById("dot-btn");
+dotBtn.addEventListener("click",function(){
+  outputInput.value += ".";
+})
+// AC button
+let acBtn=document.getElementById("ac-btn");
+acBtn.addEventListener("click",function(){
+  outputInput.value="";
+})
+// Del button
+let delBtn=document.getElementById("del-btn");
+delBtn.addEventListener("click",function(){
+  outputInput.value=outputInput.value.slice(0,-1);
+});
+// equal button
 let equalBtn=document.getElementById("equal-btn");
 equalBtn.addEventListener("click",function(){
   try{
-    outputInput.value = eval(outputInput.value);
+    outputInput.value = math.evaluate(outputInput.value);
   }catch(e){
     outputInput.value = "Error";
   }});
